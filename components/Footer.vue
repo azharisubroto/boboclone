7be0c8f19d0f071a2a62174da469396320be0aa4
Azhari Subroto <juarathemes@gmail.com>
2021-03-15 07:53:53 +0700

    full page

--- components/Footer.vue
+++ components/Footer.vue
@@ -58,12 +58,175 @@
         </v-row>
       </v-container>
     </section>
+
+    <section class="footer-widget">
+      <v-container fluid class="px-4">
+        <v-row>
+          <v-col lg="9">
+            <v-row>
+              <v-col v-for="(item, i) in footers" :key="`footer-${i}`" lg="4">
+                <h5 class="fs-12 mb-3">{{ item.title }}</h5>
+                <ul>
+                  <li v-for="(child, j) in item.items" :key="`footer-${i}-${j}`">
+                    <a :href="child.slug">{{ child.title }}</a>
+                  </li>
+                </ul>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col lg="3">
+            <h5 class="fs-12 mb-3">Follow us in</h5>
+
+            <div class="flex">
+              <v-btn icon>
+                <v-icon>mdi-instagram</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-facebook</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-pinterest</v-icon>
+              </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+
+    <section class="footer__bottom">
+      <v-container>
+        <v-row>
+          <v-col lg="12" class="text-center">
+            <span class="copyright">&copy; 2019 Bobo Choses</span>
+            <span class="d-inline-block mx-2"
+              ><a href="https://universe.bobochoses.com/legal-information">Legal Information</a></span
+            >
+            <span class="d-inline-block mx-2"
+              ><a href="https://universe.bobochoses.com/privacy-policy">Privacy Policy</a></span
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
   </footer>
 </template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      footers: [
+        {
+          title: 'Our Collections',
+          items: [
+            {
+              title: 'SS21 Baby & Kid',
+              slug: '#'
+            },
+            {
+              title: 'SS21 Adult',
+              slug: '#'
+            },
+            {
+              title: 'My account',
+              slug: '#'
+            },
+            {
+              title: 'Wholesale',
+              slug: '#'
+            }
+          ]
+        },
+        {
+          title: 'About Bobo Choses',
+          items: [
+            {
+              title: 'About',
+              slug: '#'
+            },
+            {
+              title: 'Press Contact',
+              slug: '#'
+            },
+            {
+              title: 'Store Locator',
+              slug: '#'
+            },
+            {
+              title: 'Bobo Choses Barcelona Born',
+              slug: '#'
+            },
+            {
+              title: 'Contact us',
+              slug: '#'
+            },
+            {
+              title: 'FAQ',
+              slug: '#'
+            }
+          ]
+        },
+        {
+          title: 'More information',
+          items: [
+            {
+              title: 'Payment',
+              slug: '#'
+            },
+            {
+              title: 'Delivery',
+              slug: '#'
+            },
+            {
+              title: 'Returns',
+              slug: '#'
+            },
+            {
+              title: 'Size guide',
+              slug: '#'
+            },
+            {
+              title: 'Product care',
+              slug: '#'
+            },
+            {
+              title: 'Sustainability',
+              slug: '#'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 footer {
   background: #fff;
+
+  h5 {
+    font-size: 14px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    li {
+      display: block;
+
+      a {
+        text-decoration: none;
+        font-size: 12px;
+      }
+    }
+  }
 }
 
 .features {
@@ -74,5 +237,15 @@ footer {
 .subscribe {
   background: #fff;
   padding: 30px 0;
+}
+
+.footer-widget {
+  padding: 30px 0;
+  border-top: 1px solid #d1d1d1;
+  border-bottom: 1px solid #d1d1d1;
+}
+
+.footer__bottom {
+  font-size: 12px;
 }
 </style>
